1aadd2293252ef11e98a1f974f5701a084b4cfc7
PedroSpelta <p.h.s.8989@hotmail.com>
2021-11-08 05:15:16 -0300

    adding transition on user hover to the cards

--- components/Projects/ProjectCard.jsx
+++ components/Projects/ProjectCard.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function ProjectCard() {
+function ProjectCard(props) {
+  const [isOver, setIsOver] = useState(false);
+  const { description, projLink} = props;
   return (
-    <section className="col-span border rounded-lg h-20">
-      <span>eae</span>
+    <section
+      className="col-span border rounded-lg h-20 aspect-w-16 aspect-h-9 bg-black
+      overflow-hidden backgrou relative"
+      onMouseEnter={() => setIsOver(true)}
+      onMouseLeave={() => setIsOver(false)}
+    >
+      <div className={`flex flex-col justify-center gap-8 items-center text-white text-xl font-bold absolute transition-all duration-1000 ${!isOver && "!opacity-0"}`}>
+        <span>Sobre projeto</span>
+        <button
+          className="border-white border-2 rounded-full px-6 py-1 cursor-pointer z-10"
+          onClick={() => console.log("teste")}
+        >
+          Visitar
+        </button>
+      </div>
+      <img
+        src="https://i.pinimg.com/originals/6c/e1/88/6ce1884eb448cd8e6ec31ec11ae28a0d.jpg"
+        alt=""
+        className={`transition duration-1000 opacity-100 ${
+          isOver && "!opacity-0 scale-125"
+        }`}
+      />
     </section>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 function Projects() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto mt-40 p-10 gap-5">
+    <div className="grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto mt-40 p-10 gap-5 mb-[400px]">
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
